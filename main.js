e1f33047117b95cb913f816f8fe40275ff33b92e
Yathu46 <77876350+Yathu46@users.noreply.github.com>
2021-05-07 02:23:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var first = "Yatharth";
+var last = " Puri";
+var full = first.concat(last);
+
+console.log(full);
+
+var a = 10;
+
+console.log(a);
+console.log("+");
+
+var b = 5;
+
+console.log(b);
+console.log("=");
+
+var ans = a + b;
+
+console.log(ans)
